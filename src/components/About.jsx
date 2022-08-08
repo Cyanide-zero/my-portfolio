567a30318d52a2d07e31e17bf1080304b5dccba6
Cyanide-zero <doteiy001@gmail.com>
2022-08-09 02:36:46 +0800

    Deploy Test 3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,40 +2,10 @@ import React from 'react';
 import AboutCSS from '../css/about.module.css';
 
 export default function About(){
-    const [date, setDate] = React.useState("Jan 05 2022")
-    const uDate = new Date(date)
-    const year = uDate.getFullYear();
-    const month = uDate.getMonth();
-    const day = uDate.getDate();
-    var newDate;
-
-    if(month > 8){
-        if(day>9){
-            newDate = `${year}-${month+1}-${day}`
-        }else{
-            newDate = `${year}-${month+1}-0${day}`
-        }
-        
-    }else{
-        if(day>8){
-            newDate = `${year}-0${month+1}-${day}`
-        }else{
-            newDate = `${year}-0${month+1}-0${day}`
-        }
-    }
     
     return(
         <div className={AboutCSS.container}>
-            <input
-                type="date"
-                value={newDate}
-                style={{
-                    padding:'10px',
-                    fontFamily:'sans-serif',
-                
-                }}
-            />
-            {newDate}
+            <p className={AboutCSS.title}>WAIT LANG DI PA TAPOS!!!!</p>
         </div>
     )
 

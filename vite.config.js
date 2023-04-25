6b948961fa1a4aa26ac48a360c0a93c87667b584
cyanide-zero <doteiy001@gmail.com>
2023-04-25 13:05:57 +0800

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-// @ts-check
 import reactPlugin from "vite-plugin-react";
-
-/**
- * @type { import('vite').UserConfig }
- */
 
 export default {
   base: "/my-portfolio", // set the base path to the subdirectory of your GitHub Pages site

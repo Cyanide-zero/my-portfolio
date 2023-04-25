055362974e51704aa38f745feabe7c376f27bfe8
cyanide-zero <doteiy001@gmail.com>
2023-04-25 13:01:27 +0800

    Changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import reactPlugin from "vite-plugin-react";
  */
 
 export default {
-  base: "/my-portfolio/", // set the base path to the subdirectory of your GitHub Pages site
+  base: "/my-portfolio", // set the base path to the subdirectory of your GitHub Pages site
   jsx: "react",
   plugins: [reactPlugin],
 };
